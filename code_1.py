54dce445d98b6dea884e32f5725a49f4b7158bca
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-03 17:29:28 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,58 @@
+    }
+    return data;
+}
+
+function generateRandomData() {
+
+    const data = generateRandomData();
+function main() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+
+    data.forEach(item => {
+main();
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    });public class RandomDataGenerator {
+        }
+
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+}
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+
+    public static void main(String[] args) {
+    }if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    return data
+    for item in data:
+def main():
+
+    data = generate_random_data()
+
+
+import random
+        print(f"Random Number: {item}")
+    main()
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    const data = generateRandomData();
+    }
+}
+
+function generateRandomData() {
+    const data = [];
+main();
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+
+function main() {
+    });
