1345917b57b69ede7246726f25f7477f85a0bc87
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-06 21:10:32 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,42 @@
-
-def generate_random_data():
-
-if __name__ == "__main__":
-    data = generate_random_data()
-def main():
-
-
-        print(f"Random Number: {item}")
-    main()
-    for item in data:
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-    for item in data:
-import random
-
-
-
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    return data
-def main():
-if __name__ == "__main__":    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-        console.log("Random Number: " + item);
+        }
+        int[] data = new int[10];
 }
-    for (let i = 0; i < 10; i++) {
-
-}
-function generateRandomData() {
-
+public class RandomDataGenerator {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
     }
-    const data = [];
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        for (int item : data) {
 
-    return data;
-function main() {
-main();
-    data.forEach(item => {
-    });
-    main()
-    return data
+
+            System.out.println("Random Number: " + item);    main()
         print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    return data
 import random
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
 
-    for item in data:
-if __name__ == "__main__":
-def generate_random_data():
 
 def main():
-
     data = generate_random_data()
+
+def generate_random_data():    const data = generateRandomData();
+
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+function generateRandomData() {
+}
+}
+
+    });
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    }
+        console.log("Random Number: " + item);
+
+main();
+    return data;
