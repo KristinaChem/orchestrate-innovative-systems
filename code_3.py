6888f1b6b06fb5c88e0b8212a5b7beaf430ed641
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-07 22:23:39 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,52 @@
+    return data
+import random
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+
+    main()
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+    }
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+
+        print(f"Random Number: {item}")
+    return data
+
+    data = generate_random_data()
+import random
+    main()
+def generate_random_data():
+
+if __name__ == "__main__":
+
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data
+
+    main()
+    data = generate_random_data()
