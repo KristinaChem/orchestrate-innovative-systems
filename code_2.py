eb75d553a8191fc80448b7191be951253fb75d69
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-11 01:53:20 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,52 @@
+
+        print(f"Random Number: {item}")
+    return data
 if __name__ == "__main__":
-def main():
-import random
+    for item in data:
     data = generate_random_data()
 
-    return data
     data = [random.randint(1, 100) for _ in range(10)]
+def main():
+import random
+def generate_random_data():
+
+
+    main()def generate_random_data():
+import random
 
     for item in data:
 
-        print(f"Random Number: {item}")
-def generate_random_data():
+def main():
     main()
+
+    data = generate_random_data()
+
+    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")    for item in data:
+import random
+
+
+
+    main()
+    data = generate_random_data()
+    return data
+
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]import random
+
+    data = generate_random_data()
+def generate_random_data():
+def main():
+    return data
+if __name__ == "__main__":
+    for item in data:
+
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
