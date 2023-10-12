8c90a5d4e6bd987bd63d149ae04599f873abc854
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-12 03:02:06 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,52 +1,52 @@
-    return data
-import random
-    for item in data:
-
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-def main():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
 def generate_random_data():
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
 
 
     main()
-        int[] data = new int[10];
+
+    for item in data:
+if __name__ == "__main__":
+import random            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
         for (int i = 0; i < 10; i++) {
         }
-    }
         }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-}
 public class RandomDataGenerator {
+
+        int[] data = new int[10];
+    }
         for (int item : data) {
-            System.out.println("Random Number: " + item);
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-def main():
-
-        print(f"Random Number: {item}")
-    return data
-
-    data = generate_random_data()
-import random
-    main()
-def generate_random_data():
+}
 
 if __name__ == "__main__":
 
+import random
+def main():
     for item in data:
 
-
+    data = generate_random_data()
+    main()
     data = [random.randint(1, 100) for _ in range(10)]
-import random
-if __name__ == "__main__":
-def main():
-        print(f"Random Number: {item}")
 def generate_random_data():
-    return data
+        print(f"Random Number: {item}")
+    return datadef main():
+
+        print(f"Random Number: {item}")
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+    for item in data:
 
     main()
-    data = generate_random_data()
+    return data
+def generate_random_data():
