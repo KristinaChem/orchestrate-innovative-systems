98fc6a53195e9380f6a18cc7496928cfcd303af8
KristinaChem <141217660+KristinaChem@users.noreply.github.com>
2023-10-15 06:36:04 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,64 @@
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-        print(f"Random Number: {item}")
-
-import random
-    main()
     data = generate_random_data()
 
+
+
+if __name__ == "__main__":
+
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
+def generate_random_data():
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return dataif __name__ == "__main__":
+def main():
+    data = generate_random_data()
+
+    main()
+    return data
+    for item in data:
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+
+    main()
+import random
+
+def generate_random_data():
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+def main():
+    return data            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+}
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+    }
+
+        }    main()
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
     for item in data:
 
     return data
-def main():
-if __name__ == "__main__":        int[] data = new int[10];
-    }
-        }
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
-
-    public static void main(String[] args) {
-
-        }
-}
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
